b19202d02a14b1832b3c3bc47a63e38ccb4fbd6d
Herve Nicol <12008875+hervenicol@users.noreply.github.com>
2022-06-13 12:38:46 +0200

    sync-kube-mixin.sh: reformating

--- scripts/sync-kube-mixin.sh
+++ scripts/sync-kube-mixin.sh
@@ -4,31 +4,38 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# make a temporary dir to work in
 TMPDIR="$(mktemp -d -t 'tmp.XXXXXXXXXX')"
-trap 'rm -f $TMPDIR' EXIT
-MIXIN_REPO="git@github.com:giantswarm/giantswarm-kubernetes-mixin.git"
-# clone a branch or tag if provided
-BRANCH=${1:-""}
+trap 'cleanup' EXIT
 
-if [[ -z "${BRANCH}" ]]; then
-    # clone the mixins repo
-    echo -e "\nCloning master branch:\n"
-    git clone --single-branch "${MIXIN_REPO}" "${TMPDIR}"/mixins
-else
-    # clone the mixins repo branch or tag
-    echo -e "\nCloning branch or tag '${BRANCH}':\n"
-    git clone --branch "${BRANCH}" --single-branch "${MIXIN_REPO}" "${TMPDIR}"/mixins
-fi
+function cleanup {
+  rm -rf "$TMPDIR"
+}
 
-# get the current commit of the mixin repo
-cd "${TMPDIR}"/mixins
-MIXIN_VER=$(git rev-parse HEAD)
-cd - > /dev/null
+function main {
+  local MIXIN_VER
+  # make a temporary dir to work in
+  local MIXIN_REPO="git@github.com:giantswarm/giantswarm-kubernetes-mixin.git"
+  # clone a branch or tag if provided
+  local BRANCH="${1:-}"
 
-RULESFILE="helm/prometheus-rules/templates/recording-rules/kubernetes-mixins.rules.yml"
+  if [[ -z "$BRANCH" ]]; then
+      # clone the mixins repo
+      echo -e "\nCloning master branch:\n"
+      git clone --single-branch "$MIXIN_REPO" "$TMPDIR"/mixins
+  else
+      # clone the mixins repo branch or tag
+      echo -e "\nCloning branch or tag '$BRANCH':\n"
+      git clone --branch "$BRANCH" --single-branch "$MIXIN_REPO" "$TMPDIR"/mixins
+  fi
 
-PRECONTENT='apiVersion: monitoring.coreos.com/v1
+  # get the current commit of the mixin repo
+  cd "$TMPDIR"/mixins
+  MIXIN_VER="$(git rev-parse HEAD)"
+  cd - > /dev/null
+
+  local RULESFILE="helm/prometheus-rules/templates/recording-rules/kubernetes-mixins.rules.yml"
+
+  local PRECONTENT='apiVersion: monitoring.coreos.com/v1
 kind: PrometheusRule
 metadata:
   labels:
@@ -38,13 +45,16 @@ metadata:
 spec:
 '
 
-# copy generated rules file
-cp "${TMPDIR}"/mixins/files/prometheus-rules/rules.yml "${RULESFILE}"
+  # copy generated rules file
+  cp "$TMPDIR"/mixins/files/prometheus-rules/rules.yml "$RULESFILE"
 
-# prepend K8s objectmeta to the rules file
-printf '%s  %s' "${PRECONTENT}" "$(cat "${RULESFILE}")" >"${RULESFILE}"
+  # prepend K8s objectmeta to the rules file
+  printf '%s  %s' "$PRECONTENT" "$(cat "$RULESFILE")" > "$RULESFILE"
 
-echo -e "\nSynced mixin repo at commit: ${MIXIN_VER}\n"
+  echo -e "\nSynced mixin repo at commit: $MIXIN_VER\n"
 
-# tidy up
-rm -rf "${TMPDIR}"
+  # tidy up
+  cleanup
+}
+
+main "$@"

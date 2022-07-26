23e4738893171eb669c378b0679ff0bef35fff19
Hervé Nicol <hervenicol@users.noreply.github.com>
2022-07-26 07:26:18 +0200

    Test scripts updates and fixes (#417)

--- test/hack/bin/fetch-tools.sh
+++ test/hack/bin/fetch-tools.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # let's have reproducable tests - pin to specific versions
 ARCHITECT_VERSION="6.5.0"
@@ -9,51 +10,84 @@ GIT_WORKDIR=$(git rev-parse --show-toplevel)
 
 OS_BASE="$(uname -s)"
 case "${OS_BASE}" in
-    Linux*)
-        export PROMETHEUS_SOURCE="https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz"
-        export HELM_SOURCE="https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-        export ARCHITECT_SOURCE="https://github.com/giantswarm/architect/releases/download/v${ARCHITECT_VERSION}/architect-v${ARCHITECT_VERSION}-linux-amd64.tar.gz"
-        ;;
+Linux*)
+    export PROMETHEUS_SOURCE="https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz"
+    export HELM_SOURCE="https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+    export ARCHITECT_SOURCE="https://github.com/giantswarm/architect/releases/download/v${ARCHITECT_VERSION}/architect-v${ARCHITECT_VERSION}-linux-amd64.tar.gz"
+    ;;
 
-    Darwin*)
-        export PROMETHEUS_SOURCE="https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.darwin-amd64.tar.gz"
-        export HELM_SOURCE="https://get.helm.sh/helm-v${HELM_VERSION}-darwin-amd64.tar.gz"
-        export ARCHITECT_SOURCE="https://github.com/giantswarm/architect/releases/download/v${ARCHITECT_VERSION}/architect-v${ARCHITECT_VERSION}-darwin-amd64.tar.gz"
-        ;;
-        
-    *)
-        echo "${OS_BASE} not supported - if needed, please implement by your own." 
+Darwin*)
+    export PROMETHEUS_SOURCE="https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.darwin-amd64.tar.gz"
+    export HELM_SOURCE="https://get.helm.sh/helm-v${HELM_VERSION}-darwin-amd64.tar.gz"
+    export ARCHITECT_SOURCE="https://github.com/giantswarm/architect/releases/download/v${ARCHITECT_VERSION}/architect-v${ARCHITECT_VERSION}-darwin-amd64.tar.gz"
+    ;;
+
+*)
+    echo "${OS_BASE} not supported - if needed, please implement by your own."
+    ;;
 
 esac
 
-# download files only if they don't exist yet
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/helm-${HELM_VERSION}.tar.gz ]]; then
-    curl -L -o ${GIT_WORKDIR}/test/hack/bin/helm-${HELM_VERSION}.tar.gz ${HELM_SOURCE}
-fi
+function download() {
+    local sourcefile="$1" && shift
+    local destfile="$1" && shift
 
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/prometheus-${PROMETHEUS_VERSION}.tar.gz ]]; then
-    curl -L -o ${GIT_WORKDIR}/test/hack/bin/prometheus-${PROMETHEUS_VERSION}.tar.gz ${PROMETHEUS_SOURCE}
-fi
+    # download files only if they don't exist yet
+    if [[ ! -f "$destfile" ]]; then
+        echo "Downloading $destfile"
+        curl -s -L -o "$destfile" "$sourcefile"
+    fi
 
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/architect-${ARCHITECT_VERSION}.tar.gz ]]; then
-    curl -L -o ${GIT_WORKDIR}/test/hack/bin/architect-${ARCHITECT_VERSION}.tar.gz ${ARCHITECT_SOURCE}
-fi
+    if [[ ! -f "$destfile" ]]; then
+        echo "Failed downloading $destfile"
+        return 1
+    fi
+}
 
-# extract files only if not exist yet
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/promtool ]]; then
-    tar -tzvf ${GIT_WORKDIR}/test/hack/bin/prometheus-${PROMETHEUS_VERSION}.tar.gz
-    tar -xvf ${GIT_WORKDIR}/test/hack/bin/prometheus-${PROMETHEUS_VERSION}.tar.gz -C ${GIT_WORKDIR}/test/hack/bin/ --strip-components=1 --wildcards "prometheus-${PROMETHEUS_VERSION}*/promtool"
-fi
+function extract() {
+    local binfile="$1" && shift
+    local tarfile="$1" && shift
+    local sourceurl="$1" && shift
+    local wildcards="$1" && shift
 
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/helm ]]; then
-    tar -tzvf ${GIT_WORKDIR}/test/hack/bin/helm-${HELM_VERSION}.tar.gz
-    tar -xvf ${GIT_WORKDIR}/test/hack/bin/helm-${HELM_VERSION}.tar.gz -C ${GIT_WORKDIR}/test/hack/bin/ --strip-components=1 --wildcards "*/helm"
-fi
+    # extract files only if not exist yet
+    if [[ ! -f "$binfile" ]]; then
 
-if [[ ! -f ${GIT_WORKDIR}/test/hack/bin/architect ]]; then
-    tar -tzvf ${GIT_WORKDIR}/test/hack/bin/architect-${ARCHITECT_VERSION}.tar.gz
-    tar -xvf ${GIT_WORKDIR}/test/hack/bin/architect-${ARCHITECT_VERSION}.tar.gz -C ${GIT_WORKDIR}/test/hack/bin/ --strip-components=1 --wildcards "architect-v${ARCHITECT_VERSION}-*/architect"
-fi
+        download "$sourceurl" "$tarfile"
 
+        echo ""
+        echo "## Contents for $tarfile:"
+        tar -tzvf "$tarfile"
 
+        echo "## Extracted files:"
+        tar -xvf "$tarfile" \
+            -C "$GIT_WORKDIR/test/hack/bin/" \
+            --strip-components=1 \
+            --wildcards "$wildcards"
+    fi
 
+    if [[ ! -f "$binfile" ]]; then
+        echo "Failed downloading $binfile"
+        return 1
+    fi
+}
+
+function main() {
+    extract \
+        "${GIT_WORKDIR}/test/hack/bin/promtool" \
+        "$GIT_WORKDIR/test/hack/bin/prometheus-$PROMETHEUS_VERSION.tar.gz" \
+        "$PROMETHEUS_SOURCE" \
+        "prometheus-$PROMETHEUS_VERSION*/promtool"
+    extract \
+        "${GIT_WORKDIR}/test/hack/bin/helm" \
+        "${GIT_WORKDIR}/test/hack/bin/helm-${HELM_VERSION}.tar.gz" \
+        "$HELM_SOURCE" \
+        "*/helm"
+    extract \
+        "${GIT_WORKDIR}/test/hack/bin/architect" \
+        "${GIT_WORKDIR}/test/hack/bin/architect-${ARCHITECT_VERSION}.tar.gz" \
+        "$ARCHITECT_SOURCE" \
+        "architect-v${ARCHITECT_VERSION}-*/architect"
+}
+
+main "$@"

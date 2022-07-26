1fbba77592a0b4e40691fe8108469c91c91ae1b4
Mario Constanti <mario@constanti.de>
2022-07-26 09:18:46 +0200

    document

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,22 +1,17 @@
-
-.PHONY: test
-test: install-tools
-	./test/hack/bin/verify-rules.sh
-	# make target install tools 
-	# do architect generate
-	# helm template --release-name cluster-api-monitoring --namespace giantswarm ./helm/prometheus-rules -s templates/alerting-rules/capi.rules.yml
-	# write 
-
-install-tools:
-	./test/hack/bin/fetch-tools.sh
-
 .PHONY: clean-dry-run
 clean-dry-run: ## dry run for `make clean` - print all untracked files
 	@git clean -xnf
-
 
 .PHONY: clean
 clean: ## Clean the git work dir and remove all untracked files
 	# clean stage
 	@git clean -xdf
 
+##@ Testing
+
+.PHONY: test
+test: install-tools ## run unit tests for alerting rules
+	./test/hack/bin/verify-rules.sh
+
+install-tools:
+	./test/hack/bin/fetch-tools.sh

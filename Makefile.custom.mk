da7980910f899326631efb7515d3ecc1a5e57fae
Zirko <64951262+QuantumEnigmaa@users.noreply.github.com>
2023-01-12 16:30:37 +0100

    Upgrade labels check script (#595)
    * Check labels from alertmanager config file
    
    * Edit gitignore
    
    * cleaning
    
    * fix
    
    * enhancements
    
    * fix
    
    Co-authored-by: Quentin Bisson <quentin@giantswarm.io>

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -5,7 +5,7 @@ clean-dry-run: ## dry run for `make clean` - print all untracked files
 .PHONY: clean
 clean: ## Clean the git work dir and remove all untracked files
 	# clean stage
-	git clean -xdf -- test/hack/bin test/hack/output
+	git clean -xdf -- test/hack/bin test/hack/output test/hack/checkLabels
 	git checkout -- helm/prometheus-rules/Chart.yaml
 	git checkout -- helm/prometheus-rules/values.yaml
 
@@ -27,6 +27,7 @@ template-chart: install-tools
 
 test-inhibitions: install-tools template-chart
 	# test whether inhibition labels are well defined
+	./test/hack/bin/get-inhibition.sh
 	./test/hack/bin/template-chart.sh
 	cd test/hack/checkLabels; go run main.go
 

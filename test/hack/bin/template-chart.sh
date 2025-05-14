35042ea8e7d87761825e3291d1f050a7b63c3348
QuentinBisson <quentin@giantswarm.io>
2025-05-14 14:10:36 +0200

    Fix tests

--- test/hack/bin/template-chart.sh
+++ test/hack/bin/template-chart.sh
@@ -22,15 +22,7 @@ main() {
       --set="managementCluster.name=myinstall" \
       --set="managementCluster.pipeline=stable" \
       --output-dir "$GIT_WORKDIR"/test/hack/output/helm-chart/"$provider"
-
-    # Remove useless files for tests
-    rm -f "$GIT_WORKDIR"/test/hack/output/helm-chart/"$provider"/prometheus-rules/templates/grafana-agent-rules.yaml
-    rm -f "$GIT_WORKDIR"/test/hack/output/helm-chart/"$provider"/prometheus-rules/templates/grafana-agent-rules-configmap.yaml
   done
 }
 
 main "$@"
-
-#input="US/Central - 10:26 PM (CST)"
-#[[ $input =~ ([0-9]+:[0-9]+) ]]
-#echo ${BASH_REMATCH[1]}
